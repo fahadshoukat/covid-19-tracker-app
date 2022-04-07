fe4164aab90a4e6076d3ee8c7a1cf037b0e16af7
fahadshoukat <fraza6970@gmail.com>
2022-04-07 22:31:05 +0500

    create folder structure and add dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import {Cards, Chart, CountryPicker} from './components';
 
 function App() {
   return (
     <div>
       <h1>Covid-19 Tracker App</h1>
+      <Cards />
+      <Chart />
+      <CountryPicker />
     </div>
   );
 }
